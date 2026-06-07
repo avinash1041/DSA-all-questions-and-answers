f6fe5e6110493a2fb4fe46ef7a5afc0317569239
avinash jadhav <38901043+avinash1041@users.noreply.github.com>
2026-06-07 16:05:10 +0530

    Add Dutch National Flag algorithm implementation
    Implement Dutch National Flag algorithm to sort an array of 0s, 1s, and 2s.

--- dsa.py
+++ dsa.py
@@ -47,6 +47,26 @@ def bfs(start):
 
 bfs('A')
 
+
+
+mylist = [0,1,2,0,1,0,0,0,1,1,1,1,2,2,2,2,0,0,0,2,2,1,2,1,1,1,2]
+
+def natinalflagalgo(arr):
+    low,mid,high = 0,0,len(arr)-1
+    while mid<=high:
+        if arr[mid] == 0:
+            arr[low],arr[mid] = arr[mid],arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid],arr[high] = arr[high],arr[mid]
+            high -= 1
+    return arr
+        
+print(natinalflagalgo(mylist))
+
 def longest_common_prefix(strs):
     if not strs:
         return ""
